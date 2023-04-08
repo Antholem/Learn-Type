0b3352aa390995a120f8bdbaefdafc9bad949204
“Sam Manalo” <sam.manalo@supportcommunity.com>
2023-04-08 23:33:13 +0800

    Home Page Responsiveness

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,3 +16,17 @@ activePage.forEach((element) => {
         this.classList.add('active');
     });
 });
+/*
+|---------------------------------------------------------------------------------------------------------------------------------------
+| NAVIGATION TOGGLE
+|---------------------------------------------------------------------------------------------------------------------------------------
+| Function:         addClass removeClass
+| Description:      Open and Close the Navigation for Tab and Mobile View
+|---------------------------------------------------------------------------------------------------------------------------------------
+*/
+$('#menu-btn').click(function () {
+    $('nav .navigation ul').addClass('active')
+});
+$('#menu-close').click(function(){
+    $('nav .navigation ul').removeClass('active')
+});
