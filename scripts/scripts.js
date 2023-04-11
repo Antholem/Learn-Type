87d2bd4774037c92bb7ff15cb72b9bc546d4f29e
“Sam Manalo” <sam.manalo@supportcommunity.com>
2023-04-11 22:49:22 +0800

    Buttons and Textfields

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,6 +28,7 @@ activePage.forEach((element) => {
 $('#menu-btn').click(function () {
     $('nav .navigation ul').addClass('active')
 });
+
 $('#menu-close').click(function(){
     $('nav .navigation ul').removeClass('active')
 });
